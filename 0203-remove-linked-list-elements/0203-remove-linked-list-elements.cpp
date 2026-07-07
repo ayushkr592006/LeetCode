07e9f984c802a9e6a5f8c9bdf13c741b18c8411e
ayushkr592006 <singhayushkumar135@gmail.com>
2026-07-07 12:46:24 +0530

    Time: 0 ms (100%), Space: 20.5 MB (5.31%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -10,27 +10,53 @@
  */
 class Solution {
 public:
+ListNode* remove(ListNode* head, int val){
+ if(head==NULL)return NULL;
+//  if(head->val==val){
+//     prev->next=head->next;
+//     return prev->next;
+//  }
+ 
+ head->next= remove(head->next,val);
+ if(head->val==val)return head->next;
+
+ return head;
+
+}
+
+
  void solve(ListNode* head, ListNode* prev){
     prev->next=head->next; }
 
 
     ListNode* removeElements(ListNode* head, int val) {
         if(head==NULL)return head;
-        ListNode dummy(0);
-        dummy.next=head;
-        ListNode* prev=&dummy;
-      ListNode* temp=head;
-      while(temp){
+     
+ListNode dummy(0);
+dummy.next=head;
+ListNode* prev=&dummy;
+    return remove(head,val);
 
-        if(temp->val==val){ 
-            solve(temp,prev);
-            temp=prev->next;
-        }
-        else{
-            prev=temp;
-            temp=temp->next;
-        }
-      }
-return dummy.next;
+        
+
+
+
+
+//         ListNode dummy(0);
+//         dummy.next=head;
+//         ListNode* prev=&dummy;
+//       ListNode* temp=head;
+//       while(temp){
+
+//         if(temp->val==val){ 
+//             solve(temp,prev);
+//             temp=prev->next;
+//         }
+//         else{
+//             prev=temp;
+//             temp=temp->next;
+//         }
+//       }
+// return dummy.next;
     }
 };
