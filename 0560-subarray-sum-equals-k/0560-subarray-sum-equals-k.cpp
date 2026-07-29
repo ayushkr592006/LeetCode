71c068539adbcc0dbc659e43083fe3f4d96ce316
ayushkr592006 <singhayushkumar135@gmail.com>
2026-07-29 22:08:28 +0530

    Time: 61 ms (32.96%), Space: 47.8 MB (27.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -8,22 +8,17 @@ map<int,int>mpp;
 mpp[0]=1;
 int cnt=0;
 int sum=0;
-
 for(int i=0;i<n;i++){
     sum+=nums[i];
-    int p=sum-k;
-
+   int p=sum-k;
    if(mpp.find(p)!=mpp.end()){
     cnt+=mpp[p];
-    mpp[sum]++;
    }
-   else{
-     mpp[sum]++;
-   } 
+   mpp[sum]++;
+
 }
+
 return cnt;
-
-
 
     }
 };
