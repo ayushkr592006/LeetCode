2a676c3b1f55269f94af2d910ca2b418aa0071f9
ayushkr592006 <singhayushkumar135@gmail.com>
2026-08-03 11:14:10 +0530

    Time: 15 ms (15.04%), Space: 44.8 MB (48.82%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,25 +5,46 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int n=g.size();
         int m=s.size();
-        map<int,int>mpp;
-        for(int i=0;i<m;i++){
-         mpp[s[i]]++;
+//         map<int,int>mpp;
+//         for(int i=0;i<m;i++){
+//          mpp[s[i]]++;
 
-        }
-int cnt=0;
-sort(g.begin(),g.end());
-     for(int i=0;i<n;i++){
-       auto lower=mpp.lower_bound(g[i]);
+//         }
+// int cnt=0;
+// sort(g.begin(),g.end());
+//      for(int i=0;i<n;i++){
+//        auto lower=mpp.lower_bound(g[i]);
 
 
-        if(lower!=mpp.end()){
-            cnt++;
-            lower->second--;
-            if(lower->second==0)mpp.erase(lower);
-        }
+//         if(lower!=mpp.end()){
+//             cnt++;
+//             lower->second--;
+//             if(lower->second==0)mpp.erase(lower);
+//         }
           
 
-     }
+//      }
+sort(g.begin(),g.end());
+sort(s.begin(),s.end());
+int i=0;
+int j=0;
+int cnt=0;
+
+while(i<n && j<m){
+
+if(s[j]>=g[i]){
+    cnt++;
+    i++;
+    j++;
+}
+else  if(s[j]<g[i]){
+j++;
+}
+else i++;
+
+
+}
+
 
 return cnt;
 
