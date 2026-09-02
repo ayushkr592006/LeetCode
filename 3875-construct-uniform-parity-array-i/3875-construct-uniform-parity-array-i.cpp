11affe453a975c1d1182ab791e0f03f22adb469e
ayushkr592006 <singhayushkumar135@gmail.com>
2026-09-02 11:18:36 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.95%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n=nums1.size();
-        vector<int>nums2;
-        //odd ke liye
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2!=0)nums2.push_back(nums1[i]);
-            else{
-                int c=0;
-                for(int j=0;j<n;j++){
-                    if(j!=i && (nums1[i]-nums1[j])%2!=0){
-                        nums2.push_back(nums1[i]-nums1[j]);
-                        c++;
-                        break;
-                    }
+        return true;
+//         int n=nums1.size();
+//         vector<int>nums2;
+//         //odd ke liye
+//         for(int i=0;i<n;i++){
+//             if(nums1[i]%2!=0)nums2.push_back(nums1[i]);
+//             else{
+//                 int c=0;
+//                 for(int j=0;j<n;j++){
+//                     if(j!=i && (nums1[i]-nums1[j])%2!=0){
+//                         nums2.push_back(nums1[i]-nums1[j]);
+//                         c++;
+//                         break;
+//                     }
                 
                 
-            }
-            if(c==0)break;
-        }
-        }
-    if(nums2.size()==n)return true;
-            nums2.clear();
+//             }
+//             if(c==0)break;
+//         }
+//         }
+//     if(nums2.size()==n)return true;
+//             nums2.clear();
 
 
-    for(int i=0;i<n;i++){
-            if(nums1[i]%2==0)nums2.push_back(nums1[i]);
-            else{
-                int d=0;
-                for(int j=0;j<n;j++){
-                    if(j!=i && (nums1[i]-nums1[j])%2==0){
-                        nums2.push_back(nums1[i]-nums1[j]);
-                        d++;
-                        break;
-                    }
-                }
-                if(d==0)break;
-            }
-        }
+//     for(int i=0;i<n;i++){
+//             if(nums1[i]%2==0)nums2.push_back(nums1[i]);
+//             else{
+//                 int d=0;
+//                 for(int j=0;j<n;j++){
+//                     if(j!=i && (nums1[i]-nums1[j])%2==0){
+//                         nums2.push_back(nums1[i]-nums1[j]);
+//                         d++;
+//                         break;
+//                     }
+//                 }
+//                 if(d==0)break;
+//             }
+//         }
 
-if(nums2.size()==n)return true;
-return false;
+// if(nums2.size()==n)return true;
+// return false;
 
     }
 };
